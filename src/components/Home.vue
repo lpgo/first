81d99e4ea83cb2d0ce08c521e1346a4d2b71e92e
lpgo <whereisgirl@126.com>
2017-04-01 16:55:29 +0800

    purchase

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,46 +6,53 @@
     <div class="flex-container">
       <div class="menu-list">
         <mu-list>
-          <mu-list-item  title="磅房" class="menuType">
+          <mu-list-item  title="磅房" class="menuType" toggleNested nestedListClass="test">
             <mu-icon value="devices" slot="left"/>
+            <mu-list-item title="录入过磅单" slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 录入过磅单</span>
+            </mu-list-item>
+            <mu-list-item  title="打印过磅单" slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 打印过磅单</span>
+            </mu-list-item>
           </mu-list-item>
-          <mu-list-item title="录入过磅单" inset>
-          </mu-list-item>
-          <mu-list-item  title="打印过磅单" inset>
-          </mu-list-item>
-
-          <mu-list-item  title="出售" class="menuType">
+          <mu-list-item  title="出售" class="menuType"  toggleNested nestedListClass="test">
             <mu-icon value="local_shipping" slot="left"/>
+            <mu-list-item  title="录入出货单" slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 录入出货单</span>
+            </mu-list-item>
           </mu-list-item>
-          <mu-list-item  title="录入出货单" inset>
-          </mu-list-item>
-          
-          <mu-list-item  title="查询" class="menuType">
+          <mu-list-item  title="查询" class="menuType"  toggleNested nestedListClass="test">
             <mu-icon value="search" slot="left"/>
+            <mu-list-item  title="出货单查询"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 出货单查询</span>
+            </mu-list-item>
+            <mu-list-item  title="过磅单查询"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 过磅单查询</span>
+            </mu-list-item>
           </mu-list-item>
-          <mu-list-item  title="出货单查询" inset>
-          </mu-list-item>
-          <mu-list-item  title="过磅单查询" inset>
-          </mu-list-item>
-          
-          <mu-list-item  title="统计" class="menuType">
+          <mu-list-item  title="统计" class="menuType"  toggleNested nestedListClass="test">
             <mu-icon value="equalizer" slot="left"/>
+            <mu-list-item  title="根据施工单位统计"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 根据施工单位统计</span>
+            </mu-list-item>
+            <mu-list-item  title="根据司机统计"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 根据司机统计</span>
+            </mu-list-item>
+            <mu-list-item  title="当日统计"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 当日统计</span>
+            </mu-list-item>
           </mu-list-item>
-          <mu-list-item  title="根据施工单位统计" inset>
-          </mu-list-item>
-          <mu-list-item  title="根据司机统计" inset>
-          </mu-list-item>
-          <mu-list-item  title="当日统计" inset>
-          </mu-list-item>
-        
-          <mu-list-item  title="财务" class="menuType" >
+          <mu-list-item  title="财务" class="menuType"  toggleNested nestedListClass="test" >
             <mu-icon value="monetization_on" slot="left"/>
-          </mu-list-item>
-          <mu-list-item  title="材料结算" inset>
-          </mu-list-item>
-          <mu-list-item  title="运费结算" inset>
-          </mu-list-item>
-          <mu-list-item  title="混凝土结算" inset>
+            <mu-list-item  title="材料结算"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 材料结算</span>
+            </mu-list-item>
+            <mu-list-item  title="运费结算"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 运费结算</span>
+            </mu-list-item>
+            <mu-list-item  title="混凝土结算"  slot="nested" class="menuItem">
+              <span slot="title">&#10148&nbsp&nbsp&nbsp 混凝土结算</span>
+            </mu-list-item>
           </mu-list-item>
         </mu-list>
       </div>
@@ -69,5 +76,12 @@
 }
 .menuType {
   background: #efebe9;
+}
+.menuItem {
+  background: #ffffff;
+}
+
+.test {
+  padding: 0px !important;
 }
 </style>
